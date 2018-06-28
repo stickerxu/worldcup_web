3d2d8ca8986f4b5b33ad0b6ff495e432b2c05b5e
xujingyao <xujingyao@leqian.com>
2018-06-28 17:54:11 +0800

    0628

---
+++ src/main/java/com/cup/worldcup/WorldcupApplication.java
@@ -0,0 +1,12 @@
+package com.cup.worldcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldcupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldcupApplication.class, args);
+    }
+}
---
+++ src/main/java/com/cup/worldcup/controller/CountryPickController.java
@@ -0,0 +1,19 @@
+package com.cup.worldcup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CountryPickController {
+
+    @GetMapping({"/",""})
+    public String pickPage() {
+
+        return "pick";
+    }
+
+    @GetMapping("/prediction")
+    public String prediction() {
+        return "prediction";
+    }
+}
