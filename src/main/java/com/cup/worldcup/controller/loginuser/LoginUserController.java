9155e328df34bf9838b7424ee5b1964d2b5dd412
xujingyao <xujingyao@leqian.com>
2018-07-10 18:12:15 +0800

    0710

--- src/main/java/com/cup/worldcup/controller/loginuser/LoginUserController.java
+++ src/main/java/com/cup/worldcup/controller/loginuser/LoginUserController.java
@@ -3,6 +3,10 @@ package com.cup.worldcup.controller.loginuser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @Slf4j
@@ -15,7 +19,11 @@ public class LoginUserController {
 
     @GetMapping("/user/updatePass")
     public String updatePass() {
-
         return "loginuser/user_update_pass";
+    }
+
+    @PostMapping("/user/updatePassSub")
+    public void updatePassSub(HttpServletRequest request, HttpServletResponse response) {
+
     }
 }
