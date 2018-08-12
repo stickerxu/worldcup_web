86c396d56e0729057e105e4d38fcf6460e1fbb45
Sticker Mac <stickerxu@hotmail.com>
2018-08-12 09:18:00 +0800

    0812 mac

--- src/main/java/com/cup/worldcup/entity/Article.java
+++ src/main/java/com/cup/worldcup/entity/Article.java
@@ -8,11 +8,12 @@ import java.util.Date;
 
 @Getter
 @Setter
-public class Article implements Serializable {
+public class Article extends BaseEntity implements Serializable {
     private static final long serialVersionUID = 8234412686185630203L;
 
     private Integer id;
     private Integer type;
+    private Integer status;
     private String title;
     private String file_name;
     private Date create_time;
