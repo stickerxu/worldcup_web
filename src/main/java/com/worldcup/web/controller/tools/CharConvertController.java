59c75df16a5bdb95de3452cd6e9006e33a2ab255
xujingyao <xujingyao@leqian.com>
2018-08-21 18:00:09 +0800

    comp

--- src/main/java/com/worldcup/web/controller/tools/CharConvertController.java
+++ src/main/java/com/worldcup/web/controller/tools/CharConvertController.java
@@ -1,9 +1,15 @@
 package com.worldcup.web.controller.tools;
 
+import com.worldcup.web.util.PinYinUtil;
 import lombok.extern.slf4j.Slf4j;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @Slf4j
@@ -11,6 +17,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CharConvertController {
     @GetMapping({"","/"})
     public String charConver() {
-        return "tools/charConvert/index";
+        return "tools/charconvert/index";
+    }
+
+    @PostMapping("/pyConvert")
+    public @ResponseBody String pyConvert(HttpServletRequest request) {
+        String inputText = request.getParameter("inputText");
+        try {
+            String resultText = PinYinUtil.hanZiZhuanPinYin(inputText);
+            return resultText;
+        } catch (BadHanyuPinyinOutputFormatCombination e) {
+            log.error(e.getMessage(), e);
+        }
+        return null;
     }
 }
