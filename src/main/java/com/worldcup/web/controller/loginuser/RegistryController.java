bfbdf6dffe45f7ca6a09b9c620ab92432fa415ce
xujingyao <xujingyao@leqian.com>
2018-09-11 21:28:46 +0800

    注册页修改

--- src/main/java/com/worldcup/web/controller/loginuser/RegistryController.java
+++ src/main/java/com/worldcup/web/controller/loginuser/RegistryController.java
@@ -4,12 +4,14 @@ import com.worldcup.web.entity.LoginUser;
 import com.worldcup.web.service.LoginUserService;
 import com.worldcup.web.util.EncrptionUtil;
 import com.worldcup.web.util.ParameterUtil;
+import com.worldcup.web.util.ResponsePageUtil;
 import com.worldcup.web.util.VerifyCodeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +25,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @Slf4j
@@ -35,26 +39,22 @@ public class RegistryController {
         return "loginuser/registry";
     }
 
-    @PostMapping("/registrySub")
-    public String registrySub(@RequestParam("verifyCode") String verifyCode, LoginUser loginUser,
-                              HttpSession session, HttpServletResponse response, Model model) {
+    @PostMapping("/registry")
+    public @ResponseBody Map<String, Object> registrySub(@RequestParam("verifyCode") String verifyCode, LoginUser loginUser,
+                           HttpSession session) {
+        Map<String, Object> resultMap = new HashMap<>();
         //验证码校验
         String registryVerifyCode = (String) session.getAttribute("registryVerifyCode");
         if (ParameterUtil.isBlank(registryVerifyCode) || !verifyCode.equalsIgnoreCase(registryVerifyCode)) {
-            response.setContentType("text/html;charset=UTF-8");
-            try {
-                response.getWriter().write("<script>alert('出错了');</script>");
-            } catch (IOException e) {
-                log.info(e.getMessage(), e);
-            }
-            return null;
+            resultMap.put("status", 0);
+            return resultMap;
         }
         String username = loginUser.getUsername();
         //查重
         LoginUser user = loginUserService.getUserByUsername(username);
         if (user != null) {
-            model.addAttribute("message", "用户名重复啦~ 亲，请换个更有个性的用户名！");
-            return "error/error";
+            resultMap.put("status", 1);
+            return resultMap;
         }
         String password = loginUser.getPassword();
         try {
@@ -63,8 +63,11 @@ public class RegistryController {
             }
             loginUser.setInvestCode(EncrptionUtil.md5(String.valueOf(EncrptionUtil.generateSalt(6))).substring(8,24).toUpperCase());
             loginUserService.insertLoginUser(loginUser);
-            model.addAttribute("user", loginUser);
-            return "loginuser/registry_success";
+            //删除session中的验证码信息
+            session.removeAttribute("registryVerifyCode");
+            resultMap.put("status", 2);
+            resultMap.put("message", loginUser.getUsername());
+            return resultMap;
         } catch (NoSuchAlgorithmException e) {
             log.error(e.getMessage(),e);
         } catch (UnsupportedEncodingException e) {
@@ -88,7 +91,6 @@ public class RegistryController {
     public void getVerifyCode(HttpServletResponse response, HttpSession session) {
         Object[] objs = VerifyCodeUtil.createImage();
         session.setAttribute("registryVerifyCode", String.valueOf(objs[0]));
-        log.info("图片验证码为：{}", session.getAttribute("registryVerifyCode"));
         BufferedImage image = (BufferedImage) objs[1];
         try {
             response.setContentType("image/png");
@@ -97,5 +99,15 @@ public class RegistryController {
         } catch (IOException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    @GetMapping("/registry/success")
+    public String registrySuc(@RequestParam("username") String username, ModelMap modelMap) {
+        LoginUser user = loginUserService.getUserByUsername(username);
+        if (user != null) {
+            modelMap.put("user", user);
+            return "loginuser/registry_success";
+        }
+        return ResponsePageUtil.errorPage(modelMap);
     }
 }
